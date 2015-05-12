026b1d28bc297d2af16fcdebfe9dc12ad631e21a
Voronwe <Bronwe@users.noreply.github.com>
2015-05-12 10:16:47 +0200

    Started adding new command Setloginmessage

---
+++ src/de/xcraft/engelier/XcraftGate/Commands/CommandWorldSetLoginMessage.java
@@ -0,0 +1,39 @@
+package de.xcraft.engelier.XcraftGate.Commands;
+
+import java.util.List;
+
+import org.bukkit.command.CommandSender;
+
+import de.xcraft.engelier.XcraftGate.XcraftGate;
+
+public class CommandWorldSetLoginMessage extends CommandHelperWorld {
+
+	public CommandWorldSetLoginMessage(XcraftGate plugin) {
+		super(plugin);
+	}
+
+	@Override
+	public void execute(CommandSender sender, String worldName,	List<String> args) {
+		this.sender = sender;
+		
+		if (worldName == null) {
+			error("No world given.");
+			reply("Usage: /gworld setloginmessage <worldname> [message]");
+		} else if (!hasWorld(worldName)) {
+			reply("World not found: " + worldName);
+		} else {
+			Message newMessage = null;
+		
+      //use multiple arguments
+			
+			getWorld(worldName).setLoginMessage(newMessage);
+			if (newMessage == null) {
+			  reply("Reset login message on world " + worldName + ".");
+			}
+			else {
+			  reply("Login message on world " + worldName + " set to " + newMessage);
+			}
+		}
+	}
+
+}
